a7749ce1e6e421b3f4ccea2acf6827da9c826ea1
Raja Aadhithan <72070645+raja-aadhithan@users.noreply.github.com>
2021-03-16 09:22:45 +0530

    Create Declaring wires.v

---
+++ Verilog Language/Basics/Declaring wires.v
@@ -0,0 +1,17 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+	  wire and1;
+    wire and2;
+    wire or1;
+    assign and1 = a&b;
+    assign and2 = c&d;
+    assign or1 = and1|and2;
+    assign out  = or1;
+    assign out_n = ~or1;
+endmodule
