11fa61d969921f287332e84a8f1f165fa917dcbf
Raja Aadhithan <72070645+raja-aadhithan@users.noreply.github.com>
2021-03-16 11:06:54 +0530

    Create MUX and DFF 1.v

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/MUX and DFF 1.v
@@ -0,0 +1,11 @@
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+wire x;
+    assign x = L?  r_in : q_in ;
+    always @(posedge clk)
+        Q<=x;
+endmodule
